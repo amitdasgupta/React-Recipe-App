30502e60e7af7ffafb62e8e42726a2dcedc5acee
amitdasgupta <amitdsgupta007@gmail.com>
2020-03-08 14:16:17 +0530

    Made changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const APP_ID = "";
+  const APP_KEY = "";
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+  useEffect(() => {
+    console.log("use effect called");
+    getRecipies();
+  }, [query]);
+  const getRecipies = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const result = await response.json();
+    setRecipes(result.hits);
+    console.log(result.hits);
+  };
+  const handleChange = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form" onSubmit={getSearch}>
+        <input
+          type="text"
+          className="search-bar"
+          value={search}
+          onChange={handleChange}
+        />
+        <input type="submit" className="search-button" value="Search" />
+      </form>
+      {recipes.map((item, index) => {
+        return (
+          <Recipe
+            title={item.recipe.label}
+            calories={item.recipe.calories}
+            image={item.recipe.image}
+            key={index}
+            ingredients={item.recipe.ingredients}
+          />
+        );
+      })}
     </div>
   );
 }
